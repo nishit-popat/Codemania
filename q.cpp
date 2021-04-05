7f9a1995b857840144d3a5bb9676ddd21ed13e5a
unknown <manilpatel1501@gmail.com>
2021-04-05 20:51:26 +0530

    compare is done

--- q.cpp
+++ q.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main() {
-    std::cout << "Hello World!";
-    return 0;
+int main(){
+
+int ans=0;
+for(int i=1;i<=10;i++) ans+=i;
+
+cout<<ans;
 }
