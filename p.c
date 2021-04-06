1fb026f293d4790e37afd664cb228bf42f81381e
nishit-popat <nisitpopat9999@gmail.com>
2021-04-06 11:41:11 +0530

    Commit on 6th April,2021

--- p.c
+++ p.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+#include<stdio.h>
+int main(){
+  printf("hello")
 }
